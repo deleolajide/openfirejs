e25633be82e857f58a29b56a9bd0f7ebc3cb389f
olajide.dele@gmail.com <olajide.dele@gmail.com@622b8adb-cf8c-d0f4-86c2-48d8f48ef54d>
2012-03-19 17:19:27 +0000

    First working version using JiveGlobal system properties that start with "js."

--- trunk/src/org/igniterealtime/openfirejs/OpenfireJsPlugin.java
+++ trunk/src/org/igniterealtime/openfirejs/OpenfireJsPlugin.java
@@ -55,6 +55,32 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
 
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.WrappedException;
+import org.ringojs.engine.RhinoEngine;
+import org.ringojs.engine.RingoConfiguration;
+import org.ringojs.engine.RingoWrapFactory;
+import org.ringojs.engine.SyntaxError;
+import org.ringojs.repository.FileRepository;
+import org.ringojs.repository.Repository;
+import org.ringojs.repository.ZipRepository;
+import org.ringojs.security.RingoSecurityManager;
+import org.ringojs.security.SecureWrapFactory;
+import org.ringojs.util.StringUtils;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.RhinoException;
+
+import static java.lang.System.err;
+import static java.lang.System.out;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Formatter;
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintStream;
 
 
 public class OpenfireJsPlugin implements Plugin, PropertyEventListener, ClusterEventListener,  UserEventListener, SessionEventListener, PacketInterceptor, OpenfireJsConstants {
@@ -73,7 +99,12 @@ public class OpenfireJsPlugin implements Plugin, PropertyEventListener, ClusterE
     private ExecutorService executor;
 	private PrivateStorage privateStorage;
 
-	public String lastLoadedDate = null;
+
+    private RingoConfiguration config;
+    private RhinoEngine engine;
+
+	private String lastLoadedDate = null;
+	private HashMap<String, RhinoEngine> scripts = new HashMap<String, RhinoEngine>();
 
 //-------------------------------------------------------
 //
@@ -81,8 +112,9 @@ public class OpenfireJsPlugin implements Plugin, PropertyEventListener, ClusterE
 //
 //-------------------------------------------------------
 
-	public void initializePlugin(PluginManager manager, File pluginDirectory) {
+	public void initializePlugin(PluginManager manager, final File pluginDirectory) {
 		Log.info( "["+ NAME + "] Initializing OpenfireJs Plugin");
+		Log.info( "["+ NAME + "] OpenfireJs Ringo Version " + RhinoEngine.VERSION.get(0) + "." + RhinoEngine.VERSION.get(1));
 
 		this.manager = manager;
 		this.pluginDirectory = pluginDirectory;
@@ -106,10 +138,20 @@ public class OpenfireJsPlugin implements Plugin, PropertyEventListener, ClusterE
                 try {
 					startCluster();
 					createWebAppContext();
+
+					List<String> properties = JiveGlobals.getPropertyNames();
+
+					for (String propertyName : properties)
+					{
+						String propertyValue = JiveGlobals.getProperty(propertyName);
+
+						executeScript(propertyName, propertyValue);
+					}
                 }
-                catch (Exception e) {
-                    Log.error("Error initializing OpenfireJs Plugin", e);
-                }
+
+			  	catch (Exception e) {
+					Log.error("Error initializing OpenfireJs Plugin ", e);
+				}
 
                 return true;
             }
@@ -118,7 +160,112 @@ public class OpenfireJsPlugin implements Plugin, PropertyEventListener, ClusterE
         lastLoadedDate = String.valueOf(new Date());
 	}
 
-	public void destroyPlugin() {
+	private RingoConfiguration createConfig(Repository home)
+	{
+        try {
+			List<String> systemModulePath = new ArrayList<String>();
+			List<String> userModulePath = new ArrayList<String>();
+
+			systemModulePath.add("modules");
+			systemModulePath.add("packages");
+
+			String extraPath = System.getProperty("ringo.modulepath");
+
+			if (extraPath == null) {
+				extraPath = System.getenv("RINGO_MODULE_PATH");
+			}
+
+			if (extraPath != null) {
+				Collections.addAll(userModulePath, StringUtils.split(extraPath, File.pathSeparator));
+			}
+
+			config = new RingoConfiguration(home, userModulePath, systemModulePath);
+
+			boolean hasPolicy = System.getProperty("java.security.policy") != null;
+			config.setPolicyEnabled(hasPolicy);
+			config.setWrapFactory(hasPolicy ? new SecureWrapFactory() : new RingoWrapFactory());
+			config.setArguments(new String[0]);
+			config.setOptLevel(JiveGlobals.getIntProperty("rhingo.opt.level", 0));
+			config.setBootstrapScripts(new ArrayList<String>());
+			config.setDebug(false);
+			config.setVerbose(false);
+			config.setParentProtoProperties(false);
+			config.setStrictVars(true);
+			config.setReloading(true);
+
+			return config;
+
+		} catch (Exception e) {
+			Log.error("Error OpenfireJs Plugin createConfig ", e);
+			return null;
+		}
+	}
+
+
+	private void executeScript(String scriptPropertyName, String scriptPropertyValue)
+	{
+		Repository home = null;
+
+		try {
+
+			if ("js.".equals(scriptPropertyName.substring(0,3))) // script file (.js, .zip, jar)
+			{
+				String scriptPath = scriptPropertyValue;
+				String scriptName = scriptPath + File.separator + scriptPropertyName.substring(3) + ".js";
+
+				File file = new File(scriptPath);
+				home = file.isFile() && StringUtils.isZipOrJarFile(scriptPath) ? new ZipRepository(file) : new FileRepository(file);
+
+				config = createConfig(home);
+
+				if (config != null)
+				{
+					Log.info( "["+ NAME + "] Processing Ringo Script File " + scriptName);
+
+					config.setMainScript(scriptName);
+
+					engine = new RhinoEngine(config, null);
+					scripts.put(scriptPropertyName, engine);
+
+					engine.runScript(config.getMainResource(), new String[0]);
+					engine.waitTillDone();
+				}
+			}
+
+			if ("xjs.".equals(scriptPropertyName.substring(0,4))) // executeable script
+			{
+				String scriptName = scriptPropertyName.substring(4);
+				String scriptCode = scriptPropertyValue;
+
+				home = new FileRepository(pluginDirectory);
+
+				config = createConfig(home);
+
+				if (config != null)
+				{
+					Log.info( "["+ NAME + "] Processing Ringo Script Code " + scriptName);
+					Log.debug( "["+ NAME + "] Processing Ringo Script Code " + scriptName + "\n" + scriptCode);
+
+					config.setMainScript(null);
+
+					engine = new RhinoEngine(config, null);
+					scripts.put(scriptPropertyName, engine);
+
+					engine.evaluateExpression(scriptCode);
+					engine.waitTillDone();
+
+				}
+			}
+
+		} catch (Exception e) {
+
+			Log.error("Error running OpenfireJs scripts ", e);
+		}
+	}
+
+
+	public void destroyPlugin()
+	{
 		Log.info( "["+ NAME + "] unloading " + NAME + " plugin resources");
 
 		try {
@@ -127,6 +274,13 @@ public class OpenfireJsPlugin implements Plugin, PropertyEventListener, ClusterE
 			InterceptorManager.getInstance().removeInterceptor(this);
 
 			stopCluster();
+
+			for (RhinoEngine engine : scripts.values())
+			{
+				Context cx = engine.getContextFactory().enterContext();
+				cx.exit();
+			}
+
 			executor.shutdown();
 		}
 		catch (Exception e) {
@@ -303,19 +457,33 @@ public class OpenfireJsPlugin implements Plugin, PropertyEventListener, ClusterE
 
     public void propertySet(String property, Map params)
     {
+		if (scripts.containsKey(property))
+		{
+			String value = (String)params.get("value");
 
+			RhinoEngine engine = scripts.get(property);
+			Context cx = engine.getContextFactory().enterContext();
+			cx.exit();
+
+			executeScript(property, value);
+		}
     }
 
-    public void propertyDeleted(String property, Map params)
+    public void propertyDeleted(String property, Map<String, Object> params)
     {
+		if (scripts.containsKey(property))
+		{
+			RhinoEngine engine = scripts.get(property);
+			Context cx = engine.getContextFactory().enterContext();
+			cx.exit();
+		}
+    }
+
+    public void xmlPropertySet(String property, Map<String, Object> params) {
 
     }
 
-    public void xmlPropertySet(String property, Map params) {
-
-    }
-
-    public void xmlPropertyDeleted(String property, Map params) {
+    public void xmlPropertyDeleted(String property, Map<String, Object> params) {
 
     }
 }
