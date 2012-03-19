dcce1a1c00f143fb08cb18fab5dc4dab02b18095
olajide.dele <olajide.dele@gmail.com>
2012-03-19 11:47:17 +0000

    Skeleton plugin

---
+++ src/org/igniterealtime/openfirejs/OpenfireJsConstants.java
@@ -0,0 +1,13 @@
+package org.igniterealtime.openfirejs;
+
+
+public interface OpenfireJsConstants {
+
+	public static class Properties {
+
+        //public static final String OpenfireJs_PBX_COUNTRY_CODE		= "asterisk.pbx.country.code";
+
+        private Properties() {}
+    }
+
+}
